206c3be51f60f2965b0d538af3bb09da92aba560
hmar321 <hmauricio.ar@gmail.com>
2023-11-20 14:30:42 +0100

    asd

--- src/components/Tienda.vue
+++ src/components/Tienda.vue
@@ -1,11 +1,41 @@
 <template>
     <div>
-      <h1>TiendaComponent</h1>
+        <form v-on:submit.prevent="realizarPedido()">
+            <div class="mb-3" style="width: 400px;">
+                <label class="form-label">Cubo</label>
+                <select class="form-select" v-model="idcubo">
+                    <option value="{{cubo.idCubo}}" v-for=" cubo of cubos" :key="cubo">{{ cubo.marca }} {{ cubo.nombre }}</option>
+                </select>
+            </div>
+            <button class="btn btn-success" >Realizar pedido</button>
+        </form>
     </div>
-  </template>
-  <script>
-  export default {
+</template>
+<script>
+import { CuboService } from '@/services/CuboService';
+import { CompraService } from '@/services/CompraService';
+const cuboSer = new CuboService();
+const comSer = new CompraService();
+export default {
     name: "TiendaComponent",
-  };
-  </script>
+    data() {
+        return {
+            idcubo: "",
+            cubos: []
+        }
+    },
+    mounted() {
+        cuboSer.getCubos().then((res) => {
+            this.cubos = res.data;
+        });
+    }, methods: {
+        realizarPedido() {
+            var aux = parseInt(this.idcubo);
+            comSer.insertarPedido(aux).then(() => {
+                this.$router.navigate('/compras');
+            });
+        }
+    },
+};
+</script>
   
